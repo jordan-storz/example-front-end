fabb816f5ef8b8f40f149359b5948972c74a87ff
“lukemccrae” <lukemccrae@gmail.com>
2016-12-16 15:04:03 -0700

    images are showing

--- user.js
+++ user.js
@@ -2,37 +2,58 @@ $(document).ready(() => {
 
 
 
-  function parseQuery(queryString) {
-    let search = window.location.search;
-    let result = search
-      .split(/[?=]/)
-      .slice(1, 3);
-    let test = 'thing'
-    return {
-      [result[0]]: result[1]
+    function parseQuery(queryString) {
+        let search = window.location.search;
+        let result = search
+            .split(/[?=]/)
+            .slice(1, 3);
+        let test = 'thing'
+        return {
+            [result[0]]: result[1]
+        }
     }
-  }
 
-  function getUserInfo(id) {
-    var ENDPOINT = 'http://localhost:3000/user/' + id;
-    return $.get(ENDPOINT, (response) => {
-      return response;
+    function getUserInfo(id) {
+        var ENDPOINT = 'http://localhost:3000/user/' + id;
+        return $.get(ENDPOINT, (response) => {
+            return response;
+        });
+    }
+
+    function renderUser(userInfo) {
+        let source = $("#user-template").html();
+        let template = Handlebars.compile(source);
+        let context = userInfo;
+        let html = template(context);
+        $('section.users').html(html);
+        return userInfo.id;
+    }
+
+    function renderStickers(stickerInfo) {
+        let source = $("#sticker-template").html();
+        let template = Handlebars.compile(source);
+        let context = {
+            stickers: stickerInfo
+        };
+        let html = template(context);
+        $('section.stickers').html(html);
+    }
+
+    getUserInfo(parseQuery()['id']).then((user) => {
+        renderUser(user)
+        getSticker(user.id)
+            .then(function(stickers) {
+                console.log(stickers);
+                renderStickers(stickers)
+            })
     });
-  }
 
-  function renderUser(userInfo) {
-    let source = $("#user-template").html();
-    let template = Handlebars.compile(source);
-    let context = userInfo;
-    let html = template(context);
-    $('section.users').html(html);
-    return userInfo.id;
-  }
-
-getUserInfo(parseQuery()['id']).then((user) => {
-  renderUser(user)
-});
+    function getSticker(id) {
+        return $.get('http://localhost:3000/user/' + id + '/sticker', function(data) {
+            return data;
+        })
+    }
 
 
-  console.log(parseQuery());
+    console.log(parseQuery());
 });
