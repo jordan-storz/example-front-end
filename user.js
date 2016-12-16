ea4a3127bfc3a0d65757fd42fb409cfb2053d2e2
jordan-storz <jordan.storz.1@gmail.com>
2016-12-16 14:24:24 -0700

    user.html fetches and renders user info

--- user.js
+++ user.js
@@ -1,5 +1,7 @@
 $(document).ready(() => {
 
+
+
   function parseQuery(queryString) {
     let search = window.location.search;
     let result = search
@@ -10,6 +12,27 @@ $(document).ready(() => {
       [result[0]]: result[1]
     }
   }
+
+  function getUserInfo(id) {
+    var ENDPOINT = 'http://localhost:3000/user/' + id;
+    return $.get(ENDPOINT, (response) => {
+      return response;
+    });
+  }
+
+  function renderUser(userInfo) {
+    let source = $("#user-template").html();
+    let template = Handlebars.compile(source);
+    let context = userInfo;
+    let html = template(context);
+    $('section.users').html(html);
+    return userInfo.id;
+  }
+
+getUserInfo(parseQuery()['id']).then((user) => {
+  renderUser(user)
+});
+
 
   console.log(parseQuery());
 });
